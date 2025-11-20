dddd299b27b535a361fb918cb64f8edd89e41963
77anton77 <tenigog@gmail.com>
2025-11-20 18:49:53 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def start(message):
     keyboard = InlineKeyboardMarkup()
     keyboard.add(InlineKeyboardButton(
         "📅 Тест календарь", 
-        web_app=WebAppInfo(url="https://77anton77.github.io/vakhta-calendar/")
+        web_app=WebAppInfo(url="https://77anton77.github.io/vakhta-calendar-test/")
     ))
     
     bot.send_message(
